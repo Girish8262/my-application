741db80cf0c18f0fc228360265e11854561b7283
Girish Balasaheb <gbalasaheb@aldar.com>
2024-07-31 19:38:19 +0530

    updated changes

---
+++ src/Components/Navigation/NavigationBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faChartBar, faCube } from "@fortawesome/free-solid-svg-icons";
+import "./NavigationBar.css";
+
+function NavigationBar() {
+  return (
+    <nav className="global-navigation-bar">
+      <ul>
+        <li>
+          <NavLink to="/" exact activeClassName="active">
+            <FontAwesomeIcon icon={faCube} />
+            <span className="nav-title">Main</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Home" exact activeClassName="active">
+            <FontAwesomeIcon icon={faHome} />
+            <span className="nav-title">Home</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/About" exact activeClassName="active">
+            <FontAwesomeIcon icon={faChartBar} />
+            <span className="nav-title">About</span>
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavigationBar;
